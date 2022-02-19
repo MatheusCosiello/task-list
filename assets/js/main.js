f2f93314a53d716989717c4e7d91c9632019259a
MatheusCosiello <matheus.cosiello@gmail.com>
2022-02-19 07:54:16 -0300

    att 19/02
    Depois de 1 semana afastado do codigo voltamos a trabalhar
    
    abaixo lista das atualizações
    
    criando arrow function que valida valor do inputTask
    criando função que lida com valor do input
    estiliza classes nas seção UTILITARIOS no css
    mudando nome das variaveis para ingles

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,31 @@
 // pegar informação do input
 // retornar a informação na div
 
-const inputTarefa = document.querySelector(".container-task-input");    // input entrada de novas task
-const botaoAddTarefa = document.querySelector(".button-task-add");      // botão de adicionar novas tasks
-const taskList = document.querySelector("task-list");                   // locas das tarefas
+const inputElement = document.querySelector(".container-task-input");    // input entrada de novas task
+const addTaskButton = document.querySelector(".button-task-add");      // botão de adicionar novas tasks
+const taskContainer = document.querySelector("task-list");                   // locas das tarefas
 
-botaoAddTarefa.addEventListener("click", function (e) {                 // ação do botao enviar task
-    e.preventDefault();                                                 // previni atualização da pagina ao clicar
-    const inputTarefaValue = inputTarefa.value;                         // pega valor do input e salva na memorias
-   
-});
+const validateInput = () => inputElement.value.trim().length > 0;         // validando valor do input
+
+// verifica se o valor do input é valido ou não
+
+const handleAddtask = () => {
+    const inputIsValid = validateInput();
+
+    console.log(inputIsValid);
+
+    if (!inputIsValid){
+        return inputElement.classList.add("error");                  //primeiro utilizaçao de classList da minha vida
+    }
+};
+
+const handleInputChange = () =>{
+    const inputIsValid = validateInput();
+
+    if(inputIsValid){
+        return inputElement.classList.remove("error");
+    }
+};
+
+addTaskButton.addEventListener("click", () =>handleAddtask()); // previnindo evento padrao do click no botao
+inputElement.addEventListener("change", () => handleInputChange()); // previnindo evento padrao do change(troca de cor do input)
