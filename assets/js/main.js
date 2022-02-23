f255c8d530b2bce14b5acfef9d917b3f9333f077
MatheusCosiello <matheus.cosiello@gmail.com>
2022-02-23 10:02:20 -0300

    Att 23/02
    Adicionado nova <div> dentro do task-list com nome task-item atraves de automoção via js
    criando automaçao de deletação de item,adicionando suas classes e estilizando (apenas codigo estrutural)
    deletação adicionar o botao no proximo patch
    adicionando script de chamada do iconawesome no html com o icone de lixeira

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@
 
 const inputElement = document.querySelector(".container-task-input");    // input entrada de novas task
 const addTaskButton = document.querySelector(".button-task-add");      // botão de adicionar novas tasks
-const taskContainer = document.querySelector("task-list");                   // locas das tarefas
+const taskContainer = document.querySelector(".task-list");                   // locas das tarefas
 
 const validateInput = () => inputElement.value.trim().length > 0;         // validando valor do input
 
@@ -15,8 +15,22 @@ const handleAddtask = () => {
     console.log(inputIsValid);
 
     if (!inputIsValid){
-        return inputElement.classList.add("error");                  //primeiro utilizaçao de classList da minha vida
+        return inputElement.classList.add("error");                  //adiciona class error ao html
     }
+    const taskItemContainer = document.createElement("div");        // cria elemento div
+    taskItemContainer.classList.add("task-item");                   // adiciona classe na div criada
+
+    const taskContent = document.createElement("p");               // cria elemento P
+    taskContent.innerText = inputElement.value;                     // recebe o valor de inputElemente, aceita apenas string
+
+    const deleteItem = document.createElement("i");                 // cria elemento Icon I
+    deleteItem.classList.add("fa-solid");                           // adicionando class ao icone delete para chamada do site de fontes
+    deleteItem.classList.add("fa-trash-can");
+
+    taskItemContainer.appendChild(taskContent);
+    taskItemContainer.appendChild(deleteItem);
+
+    taskContainer.appendChild(taskItemContainer);
 };
 
 const handleInputChange = () =>{
@@ -29,3 +43,4 @@ const handleInputChange = () =>{
 
 addTaskButton.addEventListener("click", () =>handleAddtask()); // previnindo evento padrao do click no botao
 inputElement.addEventListener("change", () => handleInputChange()); // previnindo evento padrao do change(troca de cor do input)
+
